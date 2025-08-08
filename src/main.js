11667d19e63183378972c0bb5a932304d6baa863
Felipe Enzou <felipe.enzou@gmail.com>
2025-08-08 15:22:02 -0300

    update: instalando o dayjs e configurando ele para rodar em toda aplicação

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 "use strict";
 
+// Configuração do dayjs
+import "./lib/dayjs";
+
+// Css
 import "./styles/global.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
+
+import dayjs from "dayjs";
+
+console.log(dayjs().format("DD/MM HH:mm"));
